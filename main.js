c63a56cbf74529659827cb20cd829dd5c6842de0
Reece Martin <capt.rm.fae@gmail.com>
2019-11-29 20:59:15 -0500

    Adding some command line argument control

--- main.js
+++ main.js
@@ -2,11 +2,20 @@
 
 // Dependencies
 const axios = require('axios');
+var method = "";
 
-axios.get(process.argv[2])
+if (process.argv[2] === '-g') {
+    method = "get";
+}
+
+if (method === "get")
+    axios.get(process.argv[3])
     .then(response => {
+        console.log("Method Selected: " + method);
         console.log(response.data.explanation);
     })
     .catch(error => {
         console.log(error);
     });
+
+// Test API Call https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY'
